5924c5abce3ccb3547bf2ff12e20313d90a7d3af
Carter <tacosax001@gmail.com>
2026-04-06 16:20:36 +0930

    🚀 Initial commit - RCC Dashboard v1.0

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'rcc-v2';
+const urlsToCache = [
+  './index.html',
+  './data.json',
+  './manifest.json'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update cache when service worker is activated
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
